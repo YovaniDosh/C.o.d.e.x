c57831fa569f7cc8dd9181cf24fb1271d0d8a7a1
Johanxt <sickoyovanizu333@gmail.com>
2026-07-05 20:09:06 -0300

    modifiedd js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,46 @@
+// ===============================
+// REFERENCIAS DEL DOM
+// ===============================
+
 const taskInput = document.getElementById("taskInput");
 const addTaskButton = document.getElementById("addTaskButton");
 const taskList = document.getElementById("taskList");
 const taskCounter = document.getElementById("taskCounter");
+
+// ===============================
+// ESTADO DE LA APLICACIÓN
+// ===============================
+
 const tasks = [];
+
+// ===============================
+// EVENTOS
+// ===============================
+
 addTaskButton.addEventListener("click", addTask);
+
+// ===============================
+// FUNCIONES
+// ===============================
+
 function addTask() {
 
-    const taskText = taskInput.value;
-        if (taskText.trim() === "") {
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
 
         alert("Debes escribir una tarea.");
 
         return;
+
     }
-    console.log(taskText);
+
+    createTask(taskText);
+
+    renderTasks();
+
+    updateCounter();
+
+    clearInput();
 
 }
